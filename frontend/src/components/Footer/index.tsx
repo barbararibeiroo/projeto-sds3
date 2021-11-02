1cdb4e3833b70227111b02379e2beaafa9d11b21
Barbara Ribeiro <barbararibeiro18@gmail.com>
2021-11-02 19:48:34 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/barbara-caroline-ribeiro/" target="_blank" rel="noreferrer">Bárbara Ribeiro</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/barbaraacarolinee" target="_blank" rel="noreferrer">@barbaracarolinee</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
